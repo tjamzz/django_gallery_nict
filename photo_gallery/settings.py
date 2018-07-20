e06dbf98b84caac6a0bd5e5018c0eadae5e57ad1
medusa99 <tjamesonedm@gmail.com>
2018-07-19 19:32:08 -0700

    Setup to display html page:link fun to url and register path to template in settings

--- photo_gallery/settings.py
+++ photo_gallery/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'photo_gallery.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],#register path of template
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- photo_gallery/urls.py
+++ photo_gallery/urls.py
@@ -17,10 +17,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
+from photos.views import photo_list
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('', photo_list, name='photo_list')
+]  
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
